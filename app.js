bd5386688bce27f69045d23b023e00a4f3633b80
Jose Maria Clavijo Basañez <pclavijobasanez@gmail.com>
2026-06-14 17:53:13 +0200

    chore: Modificar hermandad de prueba para tener distinta ruta a la misma hora

--- app.js
+++ app.js
@@ -51,15 +51,16 @@ const hermandadesDB = {
         ]
     },
     "prueba": {
-        nombre: "Hermandad de Prueba (Mañana)",
-        archivoGeojson: "ruta_planificador.geojson", // Reusamos la ruta de ejemplo
+        nombre: "Hermandad de Prueba (Misma hora)",
+        archivoGeojson: "ruta_prueba.geojson", // Ruta en Plaza de España
         pasos: [
             { nombre: "Unico Paso", iconoUrl: "paso.png", offsetMinutos: 0 }
         ],
         itinerario: [
-            { nombre: "Salida", hora: new Date(2026, 2, 30, 10, 0) }, // Al día siguiente por la mañana
-            { nombre: "Mitad", hora: new Date(2026, 2, 30, 12, 0) },
-            { "nombre": "Entrada", hora: new Date(2026, 2, 30, 14, 0) }
+            { nombre: "Salida", hora: new Date(2026, 2, 29, 17, 0) },
+            { nombre: "Punto Medio", hora: new Date(2026, 2, 29, 19, 0) },
+            { nombre: "Zona Norte", hora: new Date(2026, 2, 29, 21, 0) },
+            { nombre: "Entrada", hora: new Date(2026, 2, 29, 23, 0) }
         ]
     }
 };
